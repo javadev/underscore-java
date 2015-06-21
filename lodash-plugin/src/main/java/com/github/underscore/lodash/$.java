12336622b6e785bae9d261193cf270bd7406d4a3
javadev <javadev75@gmail.com>
2015-06-21 08:29:17 +0300

    Add lodash plugin for the underscore-java library.

---
+++ lodash-plugin/src/main/java/com/github/underscore/lodash/$.java
@@ -0,0 +1,72 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright 2015 Valentyn Kolesnikov
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.github.underscore.lodash;
+
+import java.util.*;
+
+public class $<T> extends com.github.underscore.$<T> {
+
+    public $(final Iterable<T> iterable) {
+        super(iterable);
+    }
+
+    public $(final String string) {
+        super(string);
+    }
+
+    public static class Chain<T> extends com.github.underscore.$.Chain<T> {
+        public Chain(final T item) {
+            super(item);
+        }
+        public Chain(final List<T> list) {
+            super(list);
+        }
+    }
+
+    public static Chain chain(final String item) {
+        return new $.Chain<String>(item);
+    }
+
+    public static <T> Chain chain(final List<T> list) {
+        return new $.Chain<T>(list);
+    }
+
+    public static <T> Chain chain(final Set<T> list) {
+        return new $.Chain<T>(newArrayList(list));
+    }
+
+    public static <T> Chain chain(final T ... list) {
+        return new $.Chain<T>(Arrays.asList(list));
+    }
+
+    public static <T> List<List<T>> chunk(final List<T> list, final Integer count) {
+        return Collections.emptyList();
+    }
+
+    public static void main(String ... args) {
+        final String message = "Underscore-java-lodash is a lodash plugin for underscore-java.\n\n"
+            + "For docs, license, tests, and downloads, see: http://javadev.github.io/underscore-java";
+        System.out.println(message);
+    }
+}
---
+++ lodash-plugin/src/test/java/com/github/underscore/lodash/LodashTest.java
@@ -0,0 +1,61 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright 2015 Valentyn Kolesnikov
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.github.underscore.lodash;
+
+import java.util.*;
+import org.junit.Test;
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Underscore library unit test.
+ *
+ * @author Valentyn Kolesnikov
+ */
+public class LodashTest {
+
+/*
+_.chunk(['a', 'b', 'c', 'd'], 2);
+// → [['a', 'b'], ['c', 'd']]
+
+_.chunk(['a', 'b', 'c', 'd'], 3);
+// → [['a', 'b', 'c'], ['d']]
+*/
+    @Test
+    public void chunk() {
+        assertEquals("[]", $.chunk(asList("a", "b", "c", "d"), 2).toString());
+        assertEquals("[]", $.chunk(asList("a", "b", "c", "d"), 3).toString());
+    }
+
+    @Test
+    public void main() {
+        $.main(new String[] {});
+        new $(new ArrayList<String>());
+        new $("");
+        $.chain(new ArrayList<String>());
+        $.chain(new HashSet<String>());
+        $.chain(new String[] {});
+        $.chain("");
+    }
+}
