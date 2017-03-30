fcd4b11988e297bf2681d820ec77b900099ab1d6
javadev <javadev75@gmail.com>
2017-03-30 13:13:22 +0300

    Add more unit tests for the fromXml() method.

--- string-plugin/src/test/java/com/github/underscore/string/StringTest.java
+++ string-plugin/src/test/java/com/github/underscore/string/StringTest.java
@@ -1430,6 +1430,16 @@ _.repeat('abc', 0);
             $.toJson((Map<String, Object>) $.fromXml(string)));
     }
 
+    @SuppressWarnings("unchecked")
+    @Test
+    public void fetchGetXml() {
+        String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><report><time/><coords><x>3916.234</x><y>169.914</y><z>6.33</z>"
+            + "</coords><code>NMR</code><message>Another day of everyday normal regular weather, business as usual, unless "
+            + "it’s going to be like the time of the Great Paprika Mayonnaise Incident of 2014, that was some pretty nasty "
+            + "stuff.</message><varX-Rating>8</varX-Rating></report>";
+        assertEquals("NMR", (String) ((Map<String, Object>) $.fromXml(xml)).get("code"));
+    }
+
     @Test(expected = IllegalArgumentException.class)
     public void testDecodeParseXmlErr13() {
         $.fromXml("[\"abc\u0010\"]");
