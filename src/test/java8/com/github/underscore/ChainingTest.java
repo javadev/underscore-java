b3fc05a4fdcdd1b148dfb9c41ff4c3ac6b903678
Valentyn Kolesnikov <javadev75@gmail.com>
2020-01-06 06:44:36 +0200

    Change copyright.

--- src/test/java8/com/github/underscore/ChainingTest.java
+++ src/test/java8/com/github/underscore/ChainingTest.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright 2015-2019 Valentyn Kolesnikov
+ * Copyright 2015-2020 Valentyn Kolesnikov
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
