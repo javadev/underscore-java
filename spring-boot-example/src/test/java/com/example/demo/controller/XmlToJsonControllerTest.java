9bc9385fac4c7b8a02c30676a31ad647a2a0a520
Valentyn Kolesnikov <javadev75@gmail.com>
2021-05-22 06:38:32 +0300

    Added spring boot example.

---
+++ spring-boot-example/src/test/java/com/example/demo/controller/XmlToJsonControllerTest.java
@@ -0,0 +1,66 @@
+package com.example.demo.controller;
+
+import com.github.underscore.lodash.U;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+class XmlToJsonControllerTest {
+    private XmlToJsonController testObj = new XmlToJsonController();
+
+    @Test
+    void xmltojson() {
+        ResponseEntity<Map<String, Object>> result = testObj.xmltojson(U.objectBuilder().add("xml", "<a/>").build());
+        assertSame(HttpStatus.OK, result.getStatusCode());
+    }
+
+    @Test
+    void xmltojsonError() {
+        ResponseEntity<Map<String, Object>> result = testObj.xmltojson(U.objectBuilder().add("xml", "<a/>1").build());
+        assertSame(HttpStatus.BAD_REQUEST, result.getStatusCode());
+    }
+
+    @Test
+    void formatxml() {
+        ResponseEntity<Map<String, Object>> result = testObj.formatxml(U.objectBuilder().add("xml", "<a/>").build());
+        assertSame(HttpStatus.OK, result.getStatusCode());
+    }
+
+    @Test
+    void formatxmlError() {
+        ResponseEntity<Map<String, Object>> result = testObj.formatxml(U.objectBuilder().add("xml", "<a/>1").build());
+        assertSame(HttpStatus.BAD_REQUEST, result.getStatusCode());
+    }
+
+    @Test
+    void jsontoxml() {
+        ResponseEntity<Map<String, Object>> result =
+                testObj.jsontoxml(U.objectBuilder().add("json", "{\"a\":1}").build());
+        assertSame(HttpStatus.OK, result.getStatusCode());
+    }
+
+    @Test
+    void jsontoxmlError() {
+        ResponseEntity<Map<String, Object>> result =
+                testObj.jsontoxml(U.objectBuilder().add("json", "{\"a\":1}1").build());
+        assertSame(HttpStatus.BAD_REQUEST, result.getStatusCode());
+    }
+
+    @Test
+    void formatjson() {
+        ResponseEntity<Map<String, Object>> result =
+                testObj.formatjson(U.objectBuilder().add("json", "{\"a\":1}").build());
+        assertSame(HttpStatus.OK, result.getStatusCode());
+    }
+
+    @Test
+    void formatjsonError() {
+        ResponseEntity<Map<String, Object>> result =
+                testObj.formatjson(U.objectBuilder().add("json", "{\"a\":1}1").build());
+        assertSame(HttpStatus.BAD_REQUEST, result.getStatusCode());
+    }
+}
