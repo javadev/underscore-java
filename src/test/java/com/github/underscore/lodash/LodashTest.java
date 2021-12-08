2ee7a8bcc4c1a8d2b68640f564315c9697a75f3d
Valentyn Kolesnikov <javadev75@gmail.com>
2021-12-08 09:12:01 +0200

    Removed unused import.

--- src/test/java/com/github/underscore/lodash/LodashTest.java
+++ src/test/java/com/github/underscore/lodash/LodashTest.java
@@ -39,7 +39,6 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 import org.junit.Test;
 
