f696a99e34894965ce0a6f5f4b3400ebcf2494ef
Valentyn Kolesnikov <javadev75@gmail.com>
2023-04-20 20:48:14 +0300

    Added com.github.underscore.XmlBuilder

---
+++ src/main/java/com/github/underscore/XmlBuilder.java
@@ -0,0 +1,90 @@
+package com.github.underscore;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class XmlBuilder {
+    private static final String SELF_CLOSING = "-self-closing";
+    private static final String TRUE = "true";
+    private final Map<String, Object> data;
+    private String path;
+
+    XmlBuilder(String rootName) {
+        data = new LinkedHashMap<>();
+        Map<String, Object> value = new LinkedHashMap<>();
+        value.put(SELF_CLOSING, TRUE);
+        data.put(rootName, value);
+        path = rootName;
+    }
+
+    public static XmlBuilder create(String rootName) {
+        return new XmlBuilder(rootName);
+    }
+
+    public static XmlBuilder parse(String xml) {
+        Map<String, Object> xmlData = U.fromXmlMap(xml);
+        XmlBuilder xmlBuilder = new XmlBuilder(Xml.XmlValue.getMapKey(xmlData));
+        xmlBuilder.setData(xmlData);
+        return xmlBuilder;
+    }
+
+    public XmlBuilder e(String elementName) {
+        U.remove(data, path + "." + SELF_CLOSING);
+        Map<String, Object> value = new LinkedHashMap<>();
+        value.put(SELF_CLOSING, TRUE);
+        U.set(data, path + "." + elementName, value);
+        path += "." + elementName;
+        return this;
+    }
+
+    public XmlBuilder a(String attributeName, String value) {
+        U.remove(data, path + "." + SELF_CLOSING);
+        U.set(data, path + ".-" + attributeName, value);
+        return this;
+    }
+
+    public XmlBuilder t(String text) {
+        U.remove(data, path + "." + SELF_CLOSING);
+        if (Objects.nonNull(U.get(data, path + ".#text"))) {
+            U.set(data, path + ".#text1", text);
+        } else {
+            U.set(data, path + ".#text", text);
+        }
+        return this;
+    }
+
+    public XmlBuilder up() {
+        path = path.substring(0, path.lastIndexOf("."));
+        return this;
+    }
+
+    public org.w3c.dom.Document root() {
+        try {
+            return Xml.Document.createDocument(asString());
+        } catch (Exception ex) {
+            throw new IllegalArgumentException(ex);
+        }
+    }
+
+    public org.w3c.dom.Document getDocument() {
+        return root();
+    }
+
+    public String asString() {
+        return U.toXml(data);
+    }
+
+    public String xml() {
+        return U.toXml(data);
+    }
+
+    public String json() {
+        return U.toJson(data);
+    }
+
+    private void setData(Map<String, Object> newData) {
+        data.clear();
+        data.putAll(newData);
+    }
+}
---
+++ src/test/java/com/github/underscore/XmlBuilderTest.java
@@ -0,0 +1,100 @@
+package com.github.underscore;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.w3c.dom.Document;
+
+class XmlBuilderTest {
+    private static final String XML =
+            "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+                    + "<Projects>\n"
+                    + "  <java-xmlbuilder language=\"Java\" scm=\"SVN\">\n"
+                    + "    <Location type=\"URL\">http://code.google.com/p/java-xmlbuilder/</Location>\n"
+                    + "  </java-xmlbuilder>\n"
+                    + "  <JetS3t language=\"Java\" scm=\"CVS\">\n"
+                    + "    <Location type=\"URL\">http://jets3t.s3.amazonaws.com/index.html</Location>\n"
+                    + "  </JetS3t>\n"
+                    + "</Projects>";
+
+    @Test
+    void createXml() {
+        XmlBuilder xmlBuilder =
+                XmlBuilder.create("Projects")
+                        .e("java-xmlbuilder")
+                        .a("language", "Java")
+                        .a("scm", "SVN")
+                        .e("Location")
+                        .a("type", "URL")
+                        .t("http://code.google.com/p/java-xmlbuilder/")
+                        .up()
+                        .up()
+                        .e("JetS3t")
+                        .a("language", "Java")
+                        .a("scm", "CVS")
+                        .e("Location")
+                        .a("type", "URL")
+                        .t("http://jets3t.s3.amazonaws.com/index.html");
+        assertEquals(XML, xmlBuilder.asString());
+    }
+
+    @Test
+    void parse() {
+        XmlBuilder xmlBuilder = XmlBuilder.parse(XML);
+        assertEquals(XML, xmlBuilder.asString());
+    }
+
+    @Test
+    void root() {
+        XmlBuilder xmlBuilder = new XmlBuilder(null);
+        assertTrue(xmlBuilder.root() instanceof Document);
+    }
+
+    @Test
+    void rootError() {
+        class XmlBuilderCustom extends XmlBuilder {
+            public XmlBuilderCustom(String rootName) {
+                super(rootName);
+            }
+
+            @Override
+            public String asString() {
+                return "[\"abc\u0010\"]";
+            }
+        }
+        XmlBuilder xmlBuilder = new XmlBuilderCustom(null);
+        assertThrows(IllegalArgumentException.class, xmlBuilder::root);
+    }
+
+    @Test
+    void getDocument() {
+        XmlBuilder xmlBuilder = new XmlBuilder(null);
+        assertTrue(xmlBuilder.getDocument() instanceof Document);
+    }
+
+    @Test
+    void xml() {
+        XmlBuilder xmlBuilder = XmlBuilder.parse(XML);
+        assertEquals(XML, xmlBuilder.xml());
+    }
+
+    @Test
+    void json() {
+        XmlBuilder xmlBuilder = XmlBuilder.parse(XML);
+        assertEquals(U.xmlToJson(XML), xmlBuilder.json());
+    }
+
+    @Test
+    void bug21() {
+        XmlBuilder xmlBuilder = XmlBuilder.create("root");
+        xmlBuilder.e("g1").t("Styles like ").e("g2").t("bold").up().t("is supported");
+        assertEquals(
+                "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+                        + "<root>\n"
+                        + "  <g1>Styles like <g2>bold</g2>is supported</g1>\n"
+                        + "</root>",
+                xmlBuilder.asString());
+    }
+}
