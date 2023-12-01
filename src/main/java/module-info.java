a1a86bbc91c98df23a09a05a0ead141f609d7a0d
Valentyn Kolesnikov <javadev75@gmail.com>
2023-12-02 00:10:34 +0200

    Added module-info

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * Defines the Underscore serialization/deserialization API.
+ * @since 1.97
+ */
+module com.github.underscore {
+    exports com.github.underscore;
+
+    // Optional dependency on java.xml
+    requires static java.xml;
+}
