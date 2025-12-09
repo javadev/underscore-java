442a4641f157bb8f0ee4437cc78b56f80b8fa282
Valentyn Kolesnikov <javadev75@gmail.com>
2025-12-09 17:05:52 +0200

    Updated copyright year

--- examples/src/main/java/com/github/underscore/examples/Chaining.java
+++ examples/src/main/java/com/github/underscore/examples/Chaining.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright 2015-2020 Valentyn Kolesnikov <0009-0003-9608-3364@orcid.org>
+ * Copyright 2015-2026 Valentyn Kolesnikov <0009-0003-9608-3364@orcid.org>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
